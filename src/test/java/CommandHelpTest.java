690b5eba64008949ceb2cfc6f60828a079e4628c
kystttt <doghoper75@gmail.com>
2024-09-24 03:14:50 +0500

    Добавлен модульный тест на /help

---
+++ src/test/java/CommandHelpTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommandHelpTest{
+    private TextHandler textHandler;
+
+    @BeforeEach
+    public void create(){
+        textHandler = new TextHandler();
+    }
+
+    @Test
+    public void commandStartTest(){
+        textHandler.CommandStart();
+
+        String expectedMessage = """
+            Этот бот возвращает отправленное сообщение,
+            Список команд:
+            /help - Навигация по командам бота
+            /start - приветсвие пользователя
+            """;
+        assertEquals(expectedMessage, textHandler.getOutputMassage());
+    }
+}
