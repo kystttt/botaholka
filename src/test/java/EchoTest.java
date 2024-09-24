47bf11fe15416426e8ab7547b6230913abc49e70
Ivan <kuuuuushtanov@gmail.com>
2024-09-24 20:56:16 +0500

    Исправил тест с большой строкой в commandEchoTest_4

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -14,7 +14,7 @@ public class EchoTest{
      * Тест с обычной строкой
      */
     @Test
-    public void commandStartTest_1(){
+    public void commandEchotTest_1(){
         textHandler.commandEcho("123");
 
         String expectedMessage = "Вы ввели: 123";
@@ -25,7 +25,7 @@ public class EchoTest{
      * Тест со специальными символами
      */
     @Test
-    public void commandStartTest_2(){
+    public void commandEchoTest_2(){
         textHandler.commandEcho("!@#$%^&*()");
 
         String expectedMessage = "Вы ввели: !@#$%^&*()";
@@ -36,7 +36,7 @@ public class EchoTest{
      * Тест с пустой строкой
      */
     @Test
-    public void commandStartTest_3(){
+    public void commandEchoTest_3(){
         textHandler.commandEcho("");
 
         String expectedMessage = "Вы ввели: ";
@@ -50,7 +50,7 @@ public class EchoTest{
     public void commandStartTest_4(){
         textHandler.commandEcho("a".repeat(10000));
 
-        String expectedMessage = "a".repeat(10000);
+        String expectedMessage = "Вы ввели: " + "a".repeat(10000);
         assertEquals(expectedMessage, textHandler.getOutputMassage());
     }
 }
