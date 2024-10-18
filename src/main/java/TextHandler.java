5955c258e358aaf9a670c3f3ece6c62a790b4ab5
Ivan <kuuuuushtanov@gmail.com>
2024-10-18 22:15:37 +0500

    Добавлены методы в TextHandler: удаление, дублирование заказов и просмотр всех текущих

--- src/main/java/TextHandler.java
+++ src/main/java/TextHandler.java
@@ -2,67 +2,140 @@
  * Класс, методы которого обрабатывают текст
  */
 public class TextHandler {
-    private final String START_CONST = """
-                                Добро пожаловать в бот
-                                """;
-    private final String HELP_CONST = """
-            Этот бот возвращает отправленное сообщение,
-            Список команд:
-            /help - Навигация по командам бота
-            /start - приветсвие пользователя
-            """;
 
-    private final String ECHO_CONST = "Вы ввели: ";
+    private Constants constants;
 
-    private String output_message;
-    /**
-     * Метод, который работает с текстом
-     */
+    private ListOfOrders listOfOrders;
 
-    public void commandEcho(String str){
-        output_message = ECHO_CONST + str;
+    private String output_message = "";
+
+    public TextHandler(ListOfOrders listOfOrders) {
+        this.listOfOrders = listOfOrders;
     }
 
     /**
      * Команда /start в боте
      */
-    public void commandStart(){
-        output_message = START_CONST;
+    private void commandStart() {
+        output_message = constants.getConst(Constants.Types.START);
     }
 
     /**
-     * Команда /help  в боте
+     * Команда /help в боте
      */
-     public void commandHelp(){
-        output_message = HELP_CONST;
-     }
+    private void commandHelp() {
+        output_message = constants.getConst(Constants.Types.HELP);
+    }
+
+    /**
+     * Метод при вызове команды, которой нет у бота
+     */
+    private void commandWrongTypoWord() {
+        output_message = constants.getConst(Constants.Types.ERROR_COMMAND);
+    }
 
     /**
      * геттер для output_message
+     *
      * @return возвращает output_message
      */
-    public String getOutputMassage(){
-         return output_message;
-     }
+    public String getOutputMassage(String message_text, Long chat_id) {
+        logic(message_text, chat_id);
+        return output_message;
+    }
 
     /**
      * Реализует логику бота
+     *
      * @param message_text переменная с текстом сообщения пользователя
      */
+    public void logic(String message_text, Long chat_id) {
 
-     public void logic(String message_text){
-         switch (message_text) {
-             case ("/help"):
-                 commandHelp();
-                 break;
+        switch (message_text) {
+            case ("/help"):
+                commandHelp();
+                break;
 
-             case ("/start"):
-                 commandStart();
-                 break;
+            case ("/start"):
+                commandStart();
+                break;
 
-             default:
-                 commandEcho(message_text);
-                 break;
-         }
-     }
+            case ("/listoforders"):
+                commandListOfOrders(chat_id);
+                break;
+
+            default:
+                //Это будет исправлено вместе с задачей на контекст диалога
+                if (message_text.startsWith("/duplicate-")) {
+                    commandDuplicate(message_text);
+                } else if (message_text.startsWith("/cancel-")) {
+                    commandCancelOrder(message_text);
+                } else {
+                    commandWrongTypoWord();
+                }
+                break;
+
+        }
+    }
+
+    /**
+     * Повторяет заказ по его id
+     */
+    private void commandDuplicate(String messageText) {
+        String msgNumber = messageText.split("-")[1];
+
+        for (Integer key : listOfOrders.getHashMap().keySet()) {
+            if (msgNumber.equals(Long.toString(
+                    listOfOrders.getHashMap().get(key).getChatId()))) {
+                listOfOrders.putOrder(listOfOrders.getHashMap().get(key));
+                output_message = "Заказ №" + listOfOrders.getHashMap().get(key).getChatId() + " продублирован ";
+                break;
+            }
+        }
+        output_message = String.format("Заказ с №%s не найден", msgNumber);
+    }
+
+    /**
+     * Удаляет заказ по его id
+     */
+    private void commandCancelOrder(String messageText) {
+        String msgNumber = messageText.split("-")[1];
+
+        for (Integer key : listOfOrders.getHashMap().keySet()) {
+            if (msgNumber.equals(Long.toString(
+                    listOfOrders.getHashMap().get(key).getChatId()))) {
+                Long deletedId = listOfOrders.getHashMap().get(key).getChatId();
+                listOfOrders.removeById(listOfOrders.getHashMap().get(key).getOrder_id());
+                output_message = "Заказ №" + deletedId + " удалён ";
+                break;
+            }
+        }
+        output_message = String.format("Заказ с №%s не найден", msgNumber);
+    }
+
+
+    /**
+     * Метод для показа текущих заказов клиента по его chat_id
+     *
+     * @param chat_id номер чата
+     */
+    private void commandListOfOrders(Long chat_id) {
+        boolean atLeastOnce = false;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Integer key : listOfOrders.getHashMap().keySet()) {
+            if (chat_id.equals(listOfOrders.getHashMap().get(key).getChatId())) {
+                stringBuilder.append(listOfOrders.getHashMap().get(key).formMessageForClient());
+                stringBuilder.append("\n");
+                atLeastOnce = true;
+            }
+        }
+
+        output_message = "Ваши заказы:\n";
+        output_message += stringBuilder.toString();
+        output_message += constants.getConst(Constants.Types.FUNCS_FOR_LIST_OF_ORDERS_BUYER);
+        if (!atLeastOnce) {
+            output_message = "У вас нету действительных заказов";
+        }
+    }
 }
