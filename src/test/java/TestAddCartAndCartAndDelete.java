402f104374fca13fe191afe9f9efa879c97d7cfb
Leonid Sevostianov <doghoper75@gmail.com>
2024-10-20 03:21:12 +0500

    Доделал тесты для /addToCart /delete и тд, исправил, чтобы все работало корректно. Добавил класс для теста команды /menu

--- src/test/java/TestAddCartAndCartAndDelete.java
+++ src/test/java/TestAddCartAndCartAndDelete.java
@@ -1,48 +1,58 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 /**
  * Тестируем три метода: добавления в корзину, удаления и просмотра корзины
  */
 public class TestAddCartAndCartAndDelete {
+
     private TextHandler textHandler;
-    private ListOfOrders testListOfOrders;
     private MenuList testMenuList;
+    private ListOfOrders testListOfOrders;
+    Long chat_id = 13245L;
+
     @BeforeEach
     public void setup() {
-        testListOfOrders = new ListOfOrders();
         testMenuList = new MenuList();
+        testListOfOrders = new ListOfOrders();
         textHandler = new TextHandler(testListOfOrders, testMenuList);
-
         testMenuList.getMenulist().put(1, "Шаурма - 250 рублей");
         testMenuList.getMenulist().put(2, "Напиток - 140 рублей");
     }
+
     @Test
     public void testAddToCart_ValidDishIndex() {
 /**
  * Проверяем добавление, удаление и вывод
  */
-        textHandler.addToCart("1");
+        testMenuList.setPrevCommand("/menu");
+        textHandler.getOutputMassage("1", chat_id);
         assertEquals(1, testMenuList.getCartSize());
         assertTrue(testMenuList.getCart().contains("1. Шаурма - 250 рублей"));
-        String output = textHandler.getOutputMassage("/menu", 123L);
-        assertTrue(output.contains("Блюдо добавлено в корзину:\n1. Шаурма - 250 рублей"));
-        textHandler.deleteFromCart("0");
+        assertEquals("Блюдо добавлено в корзину:\n1. Шаурма - 250 рублей\nПосмотреть вашу корзину /cart",
+                textHandler.getOutputMassage("1",chat_id));
+        testMenuList.setPrevCommand("/delete");
+        textHandler.getOutputMassage("0", chat_id);
+        textHandler.getOutputMassage("0", chat_id);
         assertEquals(0, testMenuList.getCartSize());
         textHandler.viewCart();
-        output = textHandler.getOutputMassage("/cart", 123L);
-        assertEquals("Корзина пуста", output);
+        assertEquals("Корзина пуста", textHandler.getOutputMassage("/cart", chat_id));
+
     }
+
     /**
      * Пытаемся добавить несуществующую блюдо
      */
     @Test
     public void testAddToCart_InvalidDishIndex() {
-        textHandler.addToCart("5");
+        testMenuList.setPrevCommand("/menu");
+        textHandler.getOutputMassage("5", chat_id);
         assertEquals(0, testMenuList.getCartSize());
-        String output = textHandler.getOutputMassage("/menu", 123L);
-        assertEquals("Ошибка: такого блюда нет в меню.", output);
+        assertEquals("Ошибка: такого блюда нет в меню.", textHandler.getOutputMassage("5", chat_id));
     }
+
     /**
      * Достаем блюдо с неправильным индексом
      */
@@ -50,7 +60,19 @@ public class TestAddCartAndCartAndDelete {
     public void testAddToCart_NonNumericInput() {
         textHandler.addToCart("abc");
         assertEquals(0, testMenuList.getCartSize());
-        String output = textHandler.getOutputMassage("/menu", 123L);
-        assertEquals("Ошибка: индекс блюда должен быть числом.", output);
+        assertEquals("Ошибка: индекс блюда должен быть числом.", textHandler.getOutputMassage("abc", chat_id));
+    }
+
+    /**
+     * Тест для команды /makeOrder
+     */
+    @Test
+    public void testMakeOrder(){
+        testMenuList.setPrevCommand("/menu");
+        textHandler.getOutputMassage("1", chat_id);
+        assertEquals("Ваш заказ сформирован", textHandler.getOutputMassage("/makeOrder",chat_id));
+        assertEquals(0, testMenuList.getCartSize());
+        textHandler.deleteFromCart("0");
+        assertEquals("Корзина пуста", textHandler.getOutputMassage("/makeOrder",chat_id));
     }
 }
---
+++ src/test/java/TestCommandMenu.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.json.simple.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Тест для команды /menu
+ */
+public class TestCommandMenu {
+    private MenuList testMenuList;
+    private ListOfOrders testListOfOrders;
+    private TextHandler textHandler;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        testMenuList = new MenuList();
+        testListOfOrders = new ListOfOrders();
+        textHandler = new TextHandler(testListOfOrders, testMenuList);
+        JSONObject menuJson = new JSONObject();
+        menuJson.put("Шаурма", 250L);
+        menuJson.put("Напиток", 140L);
+        menuJson.put("ЛюляКебаб", 170L);
+
+        try (FileWriter file = new FileWriter("src/main/resources/menu.json")) {
+            file.write(menuJson.toJSONString());
+        }
+    }
+    @Test
+    public void testMenuCalling() {
+        // Вызываем метод формирования меню
+        textHandler.menuCalling();
+        assertEquals("ЛюляКебаб - 170 рублей", testMenuList.getMenulist().get(1));
+        assertEquals("Напиток - 140 рублей", testMenuList.getMenulist().get(2));
+        assertEquals("Шаурма - 250 рублей",testMenuList.getMenulist().get(3));
+    }
+}
+
