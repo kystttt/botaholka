ab994292aa285bae68065b7af0baaf691b820c2d
Ivan <kuuuuushtanov@gmail.com>
2024-11-05 01:41:39 +0500

    добавлен Интерфейс для Order

---
+++ src/main/java/storages/Orders.java
@@ -0,0 +1,29 @@
+package storages;
+
+import order.Order;
+
+import java.util.List;
+
+public interface Orders {
+
+    /**
+     * Создание нового {@link Order} в {@link ListOfOrders}
+     * @param order новый заказ
+     */
+    void putOrder(Order order);
+
+    /**
+     * Возвращает список всех активных {@link Order}
+     */
+    List<Order> getOrders();
+
+    /**
+     * Возвращает {@link Order} по его id
+     */
+    Order get(int orderId);
+
+    /**
+     * Удаляет {@link Order} по его id
+     */
+    void remove(int id);
+}
